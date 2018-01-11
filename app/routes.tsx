9fc2b971355eccaa9cae987f6d48e7c7a07cc01f
Eddie Wang <bonjour@eddiewang.me>
2018-01-11 14:51:33 -0500

    page transitions

--- app/routes.tsx
+++ app/routes.tsx
@@ -2,22 +2,38 @@ import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Loadable from 'react-loadable'
 // import PageShell from './PageShell'
+import AnimatedSwitch from './AnimatedSwitch'
+import TransitionGroup from 'react-transition-group/TransitionGroup'
 
 import Layout from './Layout'
 import Home from 'pages/Home'
+
+const firstChild = props => {
+  const childrenArray = React.Children.toArray(props.children)
+  return childrenArray[0] || null
+}
+
+const pageRoutes = ({ location }) => {
+  console.log(location)
+  return (
+    <TransitionGroup component={firstChild}>
+      <AnimatedSwitch key={location.key} location={location}>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/get-started" component={getComponent('GetStarted')} />
+        <Route exact path="/about" component={getComponent('About')} />
+        <Route exact path="/how-it-works" component={getComponent('HowItWorks')} />
+        <Route exact path="/learn" component={getComponent('Learn')} />
+        <Route exact path="/roadmap" component={getComponent('Roadmap')} />
+      </AnimatedSwitch>
+    </TransitionGroup>
+  )
+}
 
 export default class Routes extends React.Component {
   public render() {
     return (
       <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/get-started" component={getComponent('GetStarted')} />
-          <Route exact path="/about" component={getComponent('About')} />
-          <Route exact path="/how-it-works" component={getComponent('HowItWorks')} />
-          <Route exact path="/learn" component={getComponent('Learn')} />
-          <Route exact path="/roadmap" component={getComponent('Roadmap')} />
-        </Switch>
+        <Route render={pageRoutes} />
       </Layout>
     )
   }
@@ -26,10 +42,6 @@ export default class Routes extends React.Component {
 function getComponent(page) {
   return Loadable({
     loader: () => System.import(`pages/${page}`),
-    render(loaded, props) {
-      const Component = loaded.default
-      return Component
-    },
     loading() {
       return <div style={{ height: '100vh' }} />
     }
