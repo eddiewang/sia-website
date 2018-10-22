43dae3e9bea5f0523f1a24ab482544bfbc705edd
Eddie Wang <bonjour@eddiewang.me>
2018-10-22 10:37:29 -0400

    enforce https middleware

--- server/index.js
+++ server/index.js
@@ -18,6 +18,14 @@ const secure = require('express-force-https')
 
 app.use(secure)
 
+function ensureSecure(req, res, next) {
+  if (req.secure || (isDev)) {
+    return next()
+  }
+  res.redirect('https://' + req.hostname + req.url)
+}
+
+app.all('*', ensureSecure)
 
 const addCount = async (req, res, next) => {
   if (req.url.includes('releases')) {
