f71a11a25004f59efd261c96d2bdd1768306d3bc
Eddie Wang <bonjour@eddiewang.me>
2019-12-23 10:50:45 -0800

    add 142 docs

--- server/index.js
+++ server/index.js
@@ -42,7 +42,8 @@ const addCount = async (req, res, next) => {
 }
 app.use('/static', addCount, express.static('public'))
 app.use('/docs', express.static('apiDocs'))
-app.use('/docs/v141', express.static('apiDocs'))
+app.use('/docs/v142', express.static('apiDocs'))
+app.use('/docs/v141', express.static('v141'))
 app.use('/docs/v140', express.static('v140'))
 
 app.use(bodyParser.urlencoded({ extended: false }))
